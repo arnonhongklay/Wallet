bb04d377a43025c74f24833b526d7156edd850b4
Arnon Hongklay <arnon@hongklay.com>
2018-11-26 22:10:12 +0700

    Add file src wallet

--- setup.py
+++ setup.py
@@ -12,14 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='Wallet',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Wallet for BuyWithTokens.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Arnon Hongklay',
+    author_email='arnon@hongklay.com',
+    url='https://github.com/arnonhongklay/wallet',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
